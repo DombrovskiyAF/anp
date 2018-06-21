08e0ee8db6616366d245834b5910991f7ef6d303
DombrovskiyAF <dombrovskiy.andrei@gmail.com>
2018-06-21 12:18:53 +0300

    тест для Кабирова

--- tools/krd_soft/main.cpp
+++ tools/krd_soft/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
+#include "../../anp/anppdusequence.h"
+#include "../../anp/anppdu.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Start!" << endl;
+    AnpPduSequence s;
+    s.readPcapFile("c:\\projects\\anp.git\\data\\mix.cap", 1000);
+    s.printAttr();
+
+    s.printPduData(5);
+    int pos = s.findIp(5);
+
+    cout << "Ipv4 pos= " << pos << endl;
+
+    cout << "STOP." << endl;
     return 0;
 }
 
